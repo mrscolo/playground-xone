d0dc51c2df40f456f1d388ae5c4101fcf41e93fd
amiyares <alejandromiyares@xone.es>
2021-11-09 21:43:11 +0100

    modificamos la ruta del framework

--- preview/tasks.js
+++
@@ -1,7 +0,0 @@
-importScripts("firebaseConfig.js");
-
-const dispatch = ({ data: { type } }) => {
-	if (type === "firebaseConfig") self.postMessage({ firebaseConfig, vapidKey });
-};
-
-self.addEventListener("message", dispatch);
